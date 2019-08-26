11c1e2d3d3a94a2ca76601bc1282440a53f05a97
Chase Shields <cwshields2@gmail.com>
2019-08-26 14:05:06 -0500

    updated

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const express = require('express')
-const massive = require('massive')
-const controller = require('./products_controller')
-require('dotenv').config()
+require("dotenv").config();
+const express = require("express");
+const massive = require("massive");
+const products_controller = require("./products_controller");
 
-const app = express()
+const app = express();
 
-const { SERVER_PORT, CONNECTION_STRING } = process.env
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
 
 massive(CONNECTION_STRING)
   .then(dbInstance => {
-    app.set("db", dbInstance)
+    app.set("db", dbInstance);
   })
-  .catch(err => console.log(err))
+  .catch(err => console.log(err));
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/api/products', controller.getAll)
-app.get('/api/products/:id', controller.getOne)
-app.put('/api/products/:id', controller.update)
-app.post('/api/products', controller.create)
-app.delete('/api/products/:id', controller.delete)
+app.post('/api/products', products_controller.create);
+app.get('/api/products', products_controller.getAll);
+app.get('/api/products/:id', products_controller.getOne);
+app.put('/api/products/:id', products_controller.update);
+app.delete('/api/products/:id', products_controller.delete);
 
 app.listen(SERVER_PORT, () => {
-  console.log(`Server listening on PORT: ${SERVER_PORT}.`)
-})
+  console.log(`Server listening on port ${SERVER_PORT}.`);
+});
--- products_controller.js
+++ products_controller.js
@@ -1,55 +1,60 @@
 module.exports = {
-  create: (req, res, next ) => {
-    const dbInstance = req.app.get('db')
-    const { name, description, price, image_url } = req.body
+  create: (req, res, next) => {
+    const dbInstance = req.app.get('db');
+    const { name, description, price, image_url } = req.body;
 
     dbInstance.create_product([name, description, price, image_url])
-      .then( () => res.sendStatus(200) )
-      .catch( err => {
-        res.status(500).send({errorMessage: "There was an error!"})
-        console.log(err);
-      })
+      .then(() => res.sendStatus(200))
+      .catch(err => {
+        res.status(500).send({ errorMessage: "Oops! Something went wrong. Our engineers have been informed!" });
+        console.log(err)
+      });
   },
-  getOne: (req, res, next ) => {
-    const dbInstance = req.app.get('db')
-    const { id } = req.params
+
+  getOne: (req, res, next) => {
+    const dbInstance = req.app.get('db');
+    const { id } = req.params;
 
     dbInstance.read_product(id)
-      .then( product => res.status(200).send( product ) )
-      .catch( err => {
-        res.status(500).send({errorMessage: "There was an error!"})
+      .then(product => res.status(200).send(product))
+      .catch(err => {
+        res.status(500).send({ errorMessage: "Oops! Something went wrong. Our engineers have been informed!" });
         console.log(err)
-      })
+      });
   },
-  getAll: (req, res, next ) => {
-    const dbInstance = req.app.get('db')
+
+  getAll: (req, res, next) => {
+    const dbInstance = req.app.get('db');
 
     dbInstance.read_products()
-      .then( product => res.status(200).send( product ) )
-      .catch( err => {
-        res.status(500).send({errorMessage: "There was an error!"})
+      .then(products => res.status(200).send(products))
+      .catch(err => {
+        res.status(500).send({ errorMessage: "Oops! Something went wrong. Our engineers have been informed!" });
         console.log(err)
-      })
+      });
   },
-  update: (req, res, next ) => {
-    const dbInstance = req.app.get('db')
-    const { params, query } = req
+
+  update: (req, res, next) => {
+    const dbInstance = req.app.get('db');
+    const { params, query } = req;
 
     dbInstance.update_product([params.id, query.desc])
-      .then( () => res.status(200) )
-      .catch( err => {
-        res.status(500).send({errorMessage: "There was an error!"})
+      .then(() => res.sendStatus(200))
+      .catch(err => {
+        res.status(500).send({ errorMessage: "Oops! Something went wrong. Our engineers have been informed!" });
         console.log(err)
-      })
+      });
   },
-  delete: (req, res, next ) => {
-    const dbInstance = req.app.get('db')
 
-    dbInstance.delete_product()
-      .then( () => res.status(200) )
-      .catch( err => {
-        res.status(500).send({errorMessage: "There was an error!"})
-        console.log(err);
-      })
-  },
-}
+  delete: (req, res, next) => {
+    const dbInstance = req.app.get('db');
+    const { id } = req.params;
+
+    dbInstance.delete_product(id)
+      .then(() => res.sendStatus(200))
+      .catch(err => {
+        res.status(500).send({ errorMessage: "Oops! Something went wrong. Our engineers have been informed!" });
+        console.log(err)
+      });
+  }
+};
